f4e1f2dcd850633e3bcaba2ed59918b98ba0d5dc
Avi Vajpeyi <avi.vajpeyi@gmail.com>
2025-02-19 17:07:55 +1300

    fix: remove harmonic dependancies

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -3,6 +3,12 @@ import matplotlib.pyplot as plt
 from starccato_sampler.utils import beta_spaced_samples
 
 
+def test_version():
+    from starccato_sampler import __version__
+
+    assert isinstance(__version__, str)
+
+
 def test_beta_samples(outdir):
     plt.plot(beta_spaced_samples(100, 0.3, 1))
     plt.savefig(f"{outdir}/test_beta_samples.png")
