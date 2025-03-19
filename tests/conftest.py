56897e6b63afb952e166cde15c61926c75dd09c0
Avi Vajpeyi <avi.vajpeyi@gmail.com>
2025-03-19 13:37:10 +1300

    save test reults for diffferent brarnches

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,22 @@
 import os
+import subprocess
 
 import pytest
 
 
 @pytest.fixture
-def outdir():
-    return os.path.join(os.path.dirname(__file__), "test_output")
+def outdir() -> str:
+    branch = _get_branch_name()
+    dir = os.path.join(os.path.dirname(__file__), f"test_output[{branch}]")
+    os.makedirs(dir, exist_ok=True)
+    return dir
+
+
+def _get_branch_name() -> str:
+    result = subprocess.run(
+        ["git", "rev-parse", "--abbrev-ref", "HEAD"],
+        capture_output=True,
+        text=True,
+    )
+    branch = result.stdout.strip() if result.returncode == 0 else "main"
+    return branch
